2284da6e31a27010b0f4a9b7e466a1d4290f658e
Vilander Costa <vilander.costa@gmail.com>
2026-06-03 21:47:10 -0300

    header tela objetivo

--- src/app/objetivo.tsx
+++ src/app/objetivo.tsx
@@ -1,12 +1,18 @@
 import { View, Text} from 'react-native'
 import { router } from 'expo-router'    
+import { PageHeader } from '@/components/PageHeader'
 
 export default function Objetivo (){
     return(
-        <View>  
-            <Text>
-                Objetivo!!!
-            </Text>
+        <View style={{flex:1, padding: 24, gap: 32}}>  
+            <PageHeader 
+                titulo = "Meta"
+                subtitulo='Economize para alcançar sua meta financeira.'
+                rightButton={{
+                    icon:'edit',
+                    onPress: () => console.log("editar Meta")
+                }}
+            />
         </View>
     )
 }
---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,34 @@
+import { Text, View, TouchableOpacity } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { styles } from './styles'
+import { colors } from '@/theme/colors'
+import { router } from 'expo-router'
+
+
+type Props = {
+    titulo:string,
+    subtitulo?: string,
+    rightButton?: {
+        onPress: () => void,
+        icon: keyof typeof MaterialIcons.glyphMap
+    }
+}
+
+export function PageHeader({titulo, subtitulo, rightButton}: Props){
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity activeOpacity={0.8} onPress={router.back}>
+                    <MaterialIcons name='arrow-back' size={32} color={colors.black}/>
+                </TouchableOpacity>
+                
+                <TouchableOpacity activeOpacity={0.8} onPress={rightButton?.onPress}>
+                    <MaterialIcons name={rightButton?.icon} size={24} color={colors.gray[500]}/>
+                </TouchableOpacity>
+
+            </View>
+            <Text style={styles.titulo}>{titulo}</Text>
+            <Text style={styles.subtitulo}>{subtitulo}</Text>
+        </View>
+    )
+}
